b108f883cd5d0dee7545c75b8689d3973eac0203
ryosuke shimomura <r-shimomura@esm.co.jp>
2012-05-28 23:54:54 +0900

    rails new actiblog を実行
    mvコマンドを使って既存のHTMLを生かしたアプリの作成をしてみた

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Actiblog::Application.config.secret_token = '538b0cfa52eeaf6040aecf5d15f5b2787f7b53952ae53d9404aa9924820c9126265fb8a276ed61ae4a668aef759cd8c5313d9a86038be479e7069bc10321ac58'
